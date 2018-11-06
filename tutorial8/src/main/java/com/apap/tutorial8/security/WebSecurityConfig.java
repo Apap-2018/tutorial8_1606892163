75fb4a3d0d36d617eb24d4683f23cfc864de31f9
Rico Putra Pradana <ricopradanaputra2198@gmail.com>
2018-11-06 14:30:57 +0700

    Done logout and add new role ADMIN

--- tutorial8/src/main/java/com/apap/tutorial8/security/WebSecurityConfig.java
+++ tutorial8/src/main/java/com/apap/tutorial8/security/WebSecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
@@ -19,26 +20,36 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 			.authorizeRequests()
 			.antMatchers("/css/**").permitAll()
 			.antMatchers("/js/**").permitAll()
+			.antMatchers("/car/**").hasAnyAuthority("DEALER")
+			.antMatchers("/dealer/**").hasAnyAuthority("ADMIN")
 			.anyRequest().authenticated()
 			.and()
 			.formLogin()
 			.loginPage("/login")
 			.permitAll()
 			.and()
-			.logout().logoutRequestMatcher(new AntPathRequestMatcher("logout")).logoutSuccessUrl("/login")
+			.logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout")).logoutSuccessUrl("/login")
 			.permitAll();
 	}
 	
-	@Autowired
-	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-		auth.inMemoryAuthentication()
-			.passwordEncoder(encoder())
-			.withUser("cokicoki").password(encoder().encode("enaksekali"))
-			.roles("USER");
-	}
+//	@Autowired
+//	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+//		auth.inMemoryAuthentication()
+//			.passwordEncoder(encoder())
+//			.withUser("cokicoki").password(encoder().encode("enaksekali"))
+//			.roles("USER");
+//	}
 	
 	@Bean
 	public BCryptPasswordEncoder encoder() {
 		return new BCryptPasswordEncoder();
+	}
+	
+	@Autowired
+	private UserDetailsService userDetailsService;
+	
+	@Autowired
+	public void configAuthentication(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(userDetailsService).passwordEncoder(encoder());
 	}
 }
